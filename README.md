315cfbe442beaa97cf5a39ad4691f2169d9cc988
Karn <karn@internet>
2023-03-16 08:47:35 +0000

    gpt2git WIP

--- README.md
+++ README.md
@@ -2,10 +2,23 @@ This repository is an MVP to bootstrap a self-modifying agent using GPT-4.
 
 MVP features:
 
-- provide snapshot of current codebase to GPT-4
-- apply changes from the GPT-4 API
+- Provide snapshot of the current codebase to GPT-4
+- Apply changes from the GPT-4 API
+
+Usage:
+
+1. Install the required packages using pip:
+   `pip install -r requirements.txt`
+2. Set up your OpenAI API key:
+   `echo "OPENAI_API_KEY=your_key_here" > .env`
+3. Run the main script:
+   `python main.py`
+
+Changelog:
 
 2023/03/16:
 
-- Create a simple script to make a query to OpenAI 3.5 turbo (still waiting on gpt4 access)
-- add git_to_json.py as first part of the git2gpt interface
+- Create a simple script to make a query to OpenAI 3.5 turbo (still waiting on GPT-4 access)
+- Add git_to_json.py as the first part of the git2gpt interface
+- Add interact_with_gpt function to main.py
+- Update README.md with usage instructions and additional details
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Any
 from gpt4_interface import get_gpt4_suggestions
 from git2gpt.core import get_repo_snapshot, apply_gpt_mutations, commit_changes
 import json
@@ -8,11 +9,7 @@ def extract_mutations(suggestions: str) -> List[Dict[str, Any]]:
     return json.loads(suggestions)
 
 
-if __name__ == "__main__":
-    repo_path = "."  # Use the current directory as the repository path
-
-    # 1. Provide a snapshot of the git repo in JSON to the GPT API
-    snapshot = get_repo_snapshot(repo_path)
+def interact_with_gpt(snapshot: str) -> str:
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
         {
@@ -20,13 +17,22 @@ if __name__ == "__main__":
             "content": f"Here's the repo snapshot: {snapshot}. Suggest some changes.",
         },
     ]
-    suggestions = get_gpt4_suggestions(messages)
+    return get_gpt4_suggestions(messages)
 
-    # 2. Process suggestions and extract mutations
-    mutations = extract_mutations(
-        suggestions
-    )  # You'll need to implement this function to parse the suggestions
 
-    # 3. Mutate the repository as directed and commit the changes
-    apply_gpt_mutations(repo_path, mutations)
-    commit_changes(repo_path, "Applied GPT-4 suggested changes.")
+if __name__ == "__main__":
+    repo_path = "."  # Use the current directory as the repository path
+
+    try:
+        # 1. Provide a snapshot of the git repo in JSON to the GPT API
+        snapshot = get_repo_snapshot(repo_path)
+        suggestions = interact_with_gpt(snapshot)
+
+        # 2. Process suggestions and extract mutations
+        mutations = extract_mutations(suggestions)
+
+        # 3. Mutate the repository as directed and commit the changes
+        apply_gpt_mutations(repo_path, mutations)
+        commit_changes(repo_path, "Applied GPT-4 suggested changes.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
