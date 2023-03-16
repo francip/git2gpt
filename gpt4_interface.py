35be2af8ceb21b83c48e19d6bbe4226dab98768c
Karn <karn@internet>
2023-03-16 12:06:50 +0000

    update model to gpt-4

--- gpt4_interface.py
+++ gpt4_interface.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # api key will be loaded from environment via dotenv
 import openai
 
-MODEL_NAME="gpt-3.5-turbo"  # Update to GPT-4 when available.
+MODEL_NAME = "gpt-4"
+
 
 def get_gpt4_suggestions(messages):
     response = openai.ChatCompletion.create(
