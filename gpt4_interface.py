a32cb586194df6744c0fc4944ce741ac46684f95
Karnagraha <karnagraha@protonmail.com>
2023-03-18 06:12:38 +0000

    Applied GPT-4 suggested changes: Use the tiktoken library to count and display the number of tokens used for each request and response

--- gpt4_interface.py
+++ gpt4_interface.py
@@ -1,18 +1,29 @@
 from dotenv import load_dotenv
+from tiktoken import Tokenizer
 
 load_dotenv() # this must run before openai is imported
-
-# api key will be loaded from environment via dotenv
-import openai
+import openai # this must be loaded after load_dotenv is called
 
 MODEL_NAME = "gpt-4"
 
+tokenizer = Tokenizer()
+
+
+def count_tokens(text):
+    return len(list(tokenizer.tokenize(text)))
+
 
 def get_gpt4_suggestions(messages):
+    initial_tokens = sum([count_tokens(json.dumps(msg)) for msg in messages])
+    print(f'Tokens used for the initial request: {initial_tokens}')
+
     response = openai.ChatCompletion.create(
         model=MODEL_NAME,
         messages=messages,
         max_tokens=4096,
-
     )
+
+    response_tokens = count_tokens(response['id'])
+    print(f'Tokens used for the generated response: {response_tokens}')
+
     return response["choices"][0]["message"]["content"]
