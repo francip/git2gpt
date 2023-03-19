98de9fc4c8f627bfa4dc403550674ef9a8faaeb6
Karnagraha <karnagraha@protonmail.com>
2023-03-19 16:30:51 +0000

    git2gpt: bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='git2gpt',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'openai>=0.27.2',
